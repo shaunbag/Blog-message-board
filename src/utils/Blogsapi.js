caa4c40c54b54fe4e5137f07903285b2437978d0
Shaunbag <shaunbag@hotmail.com>
2021-10-03 10:51:50 +0100

    Blogsapi url test

--- src/utils/Blogsapi.js
+++ src/utils/Blogsapi.js
@@ -1,7 +1,7 @@
 
 const BlogsApi = {
     async search() {   
-        const url = `ttps://shaunbagsmessages.herokuapp.com/messageboard/api/posts`
+        const url = `https://shaunbagsmessages.herokuapp.com/messageboard/api/posts`
         const response = await fetch(url);
             const jsonResponse = await response.json(); 
             if(jsonResponse){
